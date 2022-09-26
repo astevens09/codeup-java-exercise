2aa92c577e09bb14ca4fcd1816b3fdf926893d09
Anthony Stevens <aastevens1126@gmail.com>
2022-09-26 14:47:05 -0500

    intro to Java

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args){
+
+        System.out.println("Hello World!!!!!");
+
+        int myFavoriteNumber = 10;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Anthony";
+        System.out.println(myString);
+
+        long myNumber= 123L;
+        System.out.println(myNumber);
+    }
+}
